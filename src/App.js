8d87efd27bed18fe1f880cb7b8dd8890e1da6a83
Rehan Khan <rehangh8924@gmail.com>
2020-06-02 17:18:28 -0400

    added the contact list and some more css to the file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
+import ContactCard from "./ContactCard";
+import Ttile from "./Title"
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <>
+      <div className="Title">
+        <Ttile />
+      </div>
         <ContactCard 
-          firstName = "John"
+          name = "John Cena"
           mobileNum = "(718) 777-5059"
           workNum = "(212) 456-9391"
           email = "johnCen@gmail.com"
         />
+        <hr />
          <ContactCard 
-          firstName = "Tae"
+          name = "Tae Li"
           mobileNum = "(347) 234-1236"
           workNum = "(212) 426-9734"
           email = "taeWithu@hotmail.com"
         />
+        <hr />
          <ContactCard 
-          firstName = "Rose"
+          name = "Rose Smith"
           mobileNum = "(646) 519-3343"
           workNum = "(212) 127-7554"
           email = "rosie@yahoo.com"
