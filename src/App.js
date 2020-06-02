2016c8c1bc037c01ce0e9147d26859871c151d68
Rehan Khan <rehangh8924@gmail.com>
2020-06-02 15:51:28 -0400

    put what is meant to go on the contact cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <>
+        <ContactCard 
+          firstName = "John"
+          mobileNum = "(718) 777-5059"
+          workNum = "(212) 456-9391"
+          email = "johnCen@gmail.com"
+        />
+         <ContactCard 
+          firstName = "Tae"
+          mobileNum = "(347) 234-1236"
+          workNum = "(212) 426-9734"
+          email = "taeWithu@hotmail.com"
+        />
+         <ContactCard 
+          firstName = "Rose"
+          mobileNum = "(646) 519-3343"
+          workNum = "(212) 127-7554"
+          email = "rosie@yahoo.com"
+        />
+      </>
+    );
+  }
 }
 
 export default App;
